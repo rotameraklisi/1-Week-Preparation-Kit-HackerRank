57c2b339008a9554906818f91c100fcb195f6cad
rotameraklisi <snrkabatas@hotmail.com>
2022-06-15 12:53:51 +0300

    Update Mini-Max Sum.java

--- Mini-Max Sum.java
+++ Mini-Max Sum.java
@@ -16,6 +16,9 @@ class Result {
      * Complete the 'miniMaxSum' function below.
      *
      * The function accepts INTEGER_ARRAY arr as parameter.
+     * -1 DAY-
+     *Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+     *(The output can be greater than a 32 bit integer.)
      */
 
     public static void miniMaxSum(List<Integer> arr) {
